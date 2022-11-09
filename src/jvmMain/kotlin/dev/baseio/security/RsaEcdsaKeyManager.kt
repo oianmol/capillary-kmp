bd2e5bcb92dd22ff6d097a6042e68c5ba80be69e
Anmol Verma <anmol.verma4@gmail.com>
2022-11-09 16:05:16 +0530

    fix: compilation issues

--- src/jvmMain/kotlin/dev/baseio/security/RsaEcdsaKeyManager.kt
+++ src/jvmMain/kotlin/dev/baseio/security/RsaEcdsaKeyManager.kt
@@ -6,10 +6,6 @@ import com.google.crypto.tink.KeysetHandle
 import com.google.crypto.tink.PublicKeyVerify
 import dev.baseio.protoextensions.toByteArray
 import dev.baseio.slackdata.common.kmSKByteArrayElement
-<<<<<<< HEAD
-import dev.baseio.slackdata.common.kmSKByteArrayElement
-=======
->>>>>>> a86e983 (fix: compilation issues)
 import dev.baseio.slackdata.securepush.kmWrappedRsaEcdsaPublicKey
 import java.io.InputStream
 import java.security.KeyStore
@@ -20,7 +16,7 @@ import java.security.PublicKey
  * An implementation of [RsaEcdsaKeyManager] that supports RSA-ECDSA keys.
  */
 actual class RsaEcdsaKeyManager(
-    chainId: String,
+    chainId: String = "1",
     senderVerificationKey: InputStream
 ) {
     private val keychainId = "rsa_ecdsa_jvm$chainId"
@@ -39,17 +35,8 @@ actual class RsaEcdsaKeyManager(
         JVMKeyStoreRsaUtils.generateKeyPair()
     }
 
-<<<<<<< HEAD
-    actual override fun rawGenerateKeyPair() {
-        JVMKeyStoreRsaUtils.generateKeyPair(keychainId, keyStore)
-    }
-
-    actual override fun rawGetPublicKey(): ByteArray {
-        val publicKeyBytes: ByteArray = JVMKeyStoreRsaUtils.getPublicKey(keyStore, keychainId).encoded
-=======
     actual fun rawGetPublicKey(): ByteArray {
         val publicKeyBytes: ByteArray = JVMKeyStoreRsaUtils.getPublicKey().encoded
->>>>>>> a86e983 (fix: compilation issues)
         return kmWrappedRsaEcdsaPublicKey {
             padding = JVMKeyStoreRsaUtils.compatibleRsaPadding.name
             keybytesList.addAll(publicKeyBytes.map {
@@ -60,22 +47,7 @@ actual class RsaEcdsaKeyManager(
         }.toByteArray()
     }
 
-<<<<<<< HEAD
-    actual fun decrypt(cipherText: ByteArray, contextInfo: ByteArray?): ByteArray? {
-        return rawGetDecrypter().decrypt(cipherText, contextInfo)
-    }
 
-    override  fun rawGetDecrypter(): HybridDecrypt {
-        val recipientPrivateKey: PrivateKey = JVMKeyStoreRsaUtils.getPrivateKey(keyStore, keychainId)
-        return RsaEcdsaHybridDecrypt.Builder()
-            .withRecipientPrivateKey(recipientPrivateKey)
-            .withSenderVerifier(senderVerifier)
-            .withPadding(JVMKeyStoreRsaUtils.compatibleRsaPadding)
-            .build()
-    }
-
-    actual override fun rawDeleteKeyPair() {
-=======
     actual fun decrypt(cipherText: ByteArray): ByteArray {
         val recipientPrivateKey: PrivateKey = JVMKeyStoreRsaUtils.getPrivateKey()
         return HybridRsaUtils.decrypt(
@@ -94,7 +66,6 @@ actual class RsaEcdsaKeyManager(
     }
 
     actual fun rawDeleteKeyPair() {
->>>>>>> a86e983 (fix: compilation issues)
         JVMKeyStoreRsaUtils.deleteKeyPair(keyStore, keychainId)
     }
 }
