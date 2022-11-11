1313fc6df96f991cbedbf11a150f6fae722e6c1b
anmol verma <anmol.verma4@gmail.com>
2022-11-11 16:07:42 +0530

    wip support: keys for individual channels

--- src/androidMain/kotlin/dev/baseio/security/KeyManager.kt
+++
@@ -1,7 +0,0 @@
-package dev.baseio.security
-
-import com.google.crypto.tink.HybridDecrypt
-
-actual abstract class KeyManager{
-    abstract fun getDecrypter(): HybridDecrypt
-}
--- src/commonMain/kotlin/dev/baseio/security/KeyManager.kt
+++
@@ -1,7 +0,0 @@
-package dev.baseio.security
-
-const val AUTH_KEY_SERIAL_NUMBER_KEY = "current_key_serial_number"
-const val RSA_ECDSA_KEYCHAIN_ID = "rsa_ecdsa_keychain"
-const val WEB_PUSH_KEYCHAIN_ID = "web_push_keychain"
-
-expect abstract class KeyManager
