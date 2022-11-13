3c1bf9bd7e7a052b3b717503820e1bbe1aca958e
anmol verma <anmol.verma4@gmail.com>
2022-11-13 11:32:24 +0530

    wip : support ios

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.github.timortel.kotlin_multiplatform_grpc_plugin.GrpcMultiplatformExte
 plugins {
     id("com.android.library")
     kotlin("multiplatform")
-    //kotlin("native.cocoapods")
+    kotlin("native.cocoapods")
     id("io.github.timortel.kotlin-multiplatform-grpc-plugin") version "0.2.2"
 }
 
@@ -28,7 +28,7 @@ kotlin {
     android{
         publishLibraryVariants("release")
     }
-/*    cocoapods {
+    cocoapods {
         version = "1.0"
         summary = ""
         homepage = ""
@@ -40,11 +40,11 @@ kotlin {
         ios.deploymentTarget = "14.1"
 
         pod("Tink", version = "~> 1.6.1", moduleName = "Tink")
-    }*/
-/*
+    }
+
     iosX64()
     iosArm64()
-    iosSimulatorArm64()*/
+    iosSimulatorArm64()
 
     sourceSets {
         val commonMain by getting {
@@ -103,7 +103,7 @@ kotlin {
                 implementation("junit:junit:4.13.2")
             }
         }
-       /* val iosX64Main by getting
+        val iosX64Main by getting
         val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
@@ -114,7 +114,7 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
-        }*/
+        }
     }
 }
 
@@ -124,14 +124,14 @@ grpcKotlinMultiplatform {
         OutputTarget.JVM,
         listOf(kotlin.sourceSets.getByName("jvmMain"), kotlin.sourceSets.getByName("androidMain"))
     )
-    /*targetSourcesMap.put(
+   targetSourcesMap.put(
         OutputTarget.IOS,
         listOf(
             kotlin.sourceSets.getByName("iosArm64Main"),
             kotlin.sourceSets.getByName("iosSimulatorArm64Main"),
             kotlin.sourceSets.getByName("iosX64Main")
         )
-    )*/
+    )
     //Specify the folders where your proto files are located, you can list multiple.
     protoSourceFolders.set(listOf(projectDir.resolve("protos/src/main/proto")))
 }
