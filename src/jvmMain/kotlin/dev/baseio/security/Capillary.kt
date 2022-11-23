1432757bceb6a760a8aadbd15c414de44f46160a
anmol verma <anmol.verma4@gmail.com>
2022-11-23 23:27:51 +0530

    fix: jvm chain id bug

--- src/jvmMain/kotlin/dev/baseio/security/Capillary.kt
+++ src/jvmMain/kotlin/dev/baseio/security/Capillary.kt
@@ -4,20 +4,20 @@ import com.google.crypto.tink.aead.AeadConfig
 import com.google.crypto.tink.signature.SignatureConfig
 
 actual class Capillary actual constructor(private val chainId: String) {
+  val keychainId = "rsa_ecdsa_jvm$chainId"
 
   actual fun initialize() {
     com.google.crypto.tink.Config.register(SignatureConfig.LATEST);
     AeadConfig.register()
-    val keychainId = "rsa_ecdsa_jvm$chainId"
     JVMKeyStoreRsaUtils.generateKeyPair(keychainId)
   }
 
   actual fun privateKey(): PrivateKey {
-    return JVMKeyStoreRsaUtils.getPrivateKey(chainId)
+    return JVMKeyStoreRsaUtils.getPrivateKey(keychainId)
   }
 
   actual fun publicKey(): PublicKey {
-    return JVMKeyStoreRsaUtils.getPublicKey(chainId)
+    return JVMKeyStoreRsaUtils.getPublicKey(keychainId)
   }
 
   actual fun encrypt(byteArray: ByteArray, publicKey: PublicKey): ByteArray {
