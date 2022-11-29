777107248ba41e51668395fabf628c522eb8835b
anmol verma <anmol.verma4@gmail.com>
2022-11-29 11:18:34 +0530

    update: EncryptedData.swift

--- src/iosMain/kotlin/dev/baseio/security/CipherWrapper.kt
+++ src/iosMain/kotlin/dev/baseio/security/CipherWrapper.kt
@@ -2,22 +2,26 @@ package dev.baseio.security
 
 import dev.baseio.extensions.toByteArrayFromNSData
 import dev.baseio.extensions.toData
+import kotlinx.cinterop.autoreleasepool
 
 class CipherWrapper {
 
   fun encrypt(messageData: ByteArray, key: ByteArray): EncryptedData {
-    val encryptedResponse = cocoapods.capillaryslack.CapillaryIOS.encryptWithData(messageData.toData(), key.toData())
-
-    return EncryptedData(
-      encryptedResponse.first()!!.toByteArrayFromNSData(),
-      encryptedResponse.second()!!.toByteArrayFromNSData()
-    )
+    autoreleasepool {
+      val encryptedResponse = cocoapods.capillaryslack.CapillaryIOS.encryptWithData(messageData.toData(), key.toData())
+      return EncryptedData(
+        encryptedResponse.firstItem()?.toByteArrayFromNSData() ?: ByteArray(0),
+        encryptedResponse.secondItem()?.toByteArrayFromNSData() ?: ByteArray(0)
+      )
+    }
   }
 
   fun decrypt(symmetricKeyCiphertext: ByteArray, payloadCiphertext: ByteArray, key: ByteArray): ByteArray {
-    return cocoapods.capillaryslack.CapillaryIOS.decryptWithSymmetricKeyCiphertext(
-      symmetricKeyCiphertext.toData(), payloadCiphertext.toData(),
-      key.toData()
-    )!!.toByteArrayFromNSData()
+    autoreleasepool {
+      return cocoapods.capillaryslack.CapillaryIOS.decryptWithSymmetricKeyCiphertext(
+        symmetricKeyCiphertext.toData(), payloadCiphertext.toData(),
+        key.toData()
+      )!!.toByteArrayFromNSData()
+    }
   }
 }
