60ea2e83fbb85186844a030b0b77c31cd25a998c
anmol verma <anmol.verma4@gmail.com>
2022-11-29 09:43:14 +0530

    refactor: make CapillaryInstances.getInstance synchrozied with Mutex lock

--- src/commonTest/kotlin/TestCapillaryInitialization.kt
+++ src/commonTest/kotlin/TestCapillaryInitialization.kt
@@ -1,4 +1,5 @@
 import dev.baseio.security.CapillaryInstances
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -6,14 +7,16 @@ import kotlin.test.assertTrue
 class TestCapillaryInitialization {
     @Test
     fun `when user and channel keypair is created then we encr and decr works!`() {
-        with(CapillaryInstances.getInstance("anmol", isTest = true)) {
-            val publicKeyUser = publicKey()
-            assertNotNull(publicKeyUser)
-            val privateKeyUser = privateKey()
-            assertNotNull(privateKeyUser)
-            val encrypted = encrypt("Anmol".encodeToByteArray(), publicKeyUser)
-            val decrypted = decrypt(encrypted, privateKeyUser)
-            assertTrue(decrypted.contentEquals("Anmol".encodeToByteArray()))
+        runBlocking {
+            with(CapillaryInstances.getInstance("anmol", isTest = true)) {
+                val publicKeyUser = publicKey()
+                assertNotNull(publicKeyUser)
+                val privateKeyUser = privateKey()
+                assertNotNull(privateKeyUser)
+                val encrypted = encrypt("Anmol".encodeToByteArray(), publicKeyUser)
+                val decrypted = decrypt(encrypted, privateKeyUser)
+                assertTrue(decrypted.contentEquals("Anmol".encodeToByteArray()))
+            }
         }
     }
 }
